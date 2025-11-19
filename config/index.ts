0e1290ee74bd60db0c8de2d9c3b1e1a2c146031f
alakmar344 <proman007power@gmail.com>
2025-11-19 19:18:14 +0530

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,10 +3,10 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: 'esamz.ai',
+  description: 'dive into it ',
   copyright: '',
-  privacy_policy: '',
+  privacy_policy: 'https://nimble-wishbone-9de.notion.site/ebd/2a6ee712a8f9806d8c5bc4031356df10',
   default_language: 'en',
   disable_session_same_site: false, // set it to true if you want to embed the chatbot in an iframe
 }
